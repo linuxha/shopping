5b90428891f3d4cf1a28ba192ba9d5d987912811
Neil Cherry <linuxha@linuxha.com>
2023-04-08 22:14:19 -0400

    feat: add no ssl if no ssl files, cli port select, cav.sh and fav icon support
    Got a little caried away with ChatGPT4 and added:
    
    - No listen on HTTPS port if the cert and key were missing
    - ability to pass listen port selection
    - cav.sh script (empty shell for now)
    - Added favicon.ico support and an icon

--- app.js
+++ app.js
@@ -1,31 +1,76 @@
 // ==============================================================================
-const myHTTP  = 13000;
-const myHTTPS = 13443;
+// This node.js is written by GhatGPT4. I fed it my requirements and tweaked it
+// long the way.
+//
+// As a user I wanted to generate a grocery list from an invetory list of itema
+// and aisle. I wanted it as a web interface with foldable (show/hide) aisles
+// listing the aisles. I wanted the ability to add items/aisles to the inventory
+// and when I click generate a new page with the list of the items, sorted by
+// aisle would be generated in a simple Emacs org-mode text list with check
+// boxes. On my phone I have a simple org-mode app that allows me to sync via
+// webdav files. If I mv the list to the appropriate webdav server using cav.sh
+// I can then sync with my phone the generated list
 
-const express = require('express');
+const myHTTP  = process.argv[2] ? parseInt(process.argv[2]) : 13000;
+//const myHTTP  = 13000;
+//const myHTTPS = process.argv[3] ? parseInt(process.argv[3]) : 13443;
+//const myHTTPS = 13443;
+const myHTTPS = myHTTP + 443;
+
+const express    = require('express');
 const bodyParser = require('body-parser');
-const fs = require('fs');
-const app = express();
+const fs         = require('fs');
+const app        = express();
+
+const { exec } = require('child_process');
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
-//app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-const path = require('path');
-
+const path  = require('path');
 const https = require('https');
 
 // Read the SSL certificate files
+// Note: These files need to be provided by the user
+// Read the SSL certificate files
+const privateKeyPath  = './ssl/key.pem';
+const certificatePath = './ssl/cert.pem';
+// Mine are not self signed but I left this alone
+const caPath          = './ssl/cert.pem'; // For self-signed certificate, use the same cert.pem
+
+if (fs.existsSync(privateKeyPath) && fs.existsSync(certificatePath) && fs.existsSync(caPath)) {
+    const privateKey  = fs.readFileSync(privateKeyPath, 'utf8');
+    const certificate = fs.readFileSync(certificatePath, 'utf8');
+    const ca          = fs.readFileSync(caPath, 'utf8');
+
+    const credentials = {
+        key:  privateKey,
+        cert: certificate,
+        ca:   ca,
+    };
+
+    // Create an HTTPS service
+    const httpsServer = https.createServer(credentials, app);
+
+    // Set the HTTPS server to listen on a specific port (e.g., 8443)
+    httpsServer.listen(myHTTPS, () => {
+        console.log(`HTTPS server running on port ${myHTTPS}`);
+    });
+} else {
+    console.log('SSL certificate files not found. Skipping HTTPS server.');
+}
+/*
 const privateKey  = fs.readFileSync('./ssl/key.pem', 'utf8');
 const certificate = fs.readFileSync('./ssl/cert.pem', 'utf8');
+
 const ca = fs.readFileSync('./ssl/cert.pem', 'utf8'); // For self-signed certificate, use the same cert.pem
 
 const credentials = {
-  key: privateKey,
-  cert: certificate,
-  ca: ca,
+    key:  privateKey,
+    cert: certificate,
+    ca:   ca,
 };
 
 // Create an HTTPS service
@@ -33,44 +78,28 @@ const httpsServer = https.createServer(credentials, app);
 
 // Set the HTTPS server to listen on a specific port (e.g., 8443)
 httpsServer.listen(myHTTPS, () => {
-  console.log(`HTTPS server running on port ${myHTTPS}`);
+    console.log(`HTTPS server running on port ${myHTTPS}`);
 });
+*/
+// ==============================================================================
 
 /*
-Refused to apply style from 'http://mozart.uucp:13000/styles.css' because its MIME type ('text/html') is not a supported stylesheet MIME type, and strict MIME checking is enabled.
+@FIXED: Refused to apply style from 'http://mozart.uucp:13000/styles.css' because its MIME type ('text/html') is not a supported stylesheet MIME type, and strict MIME checking is enabled.
 */
 
 app.use('/styles.css', (req, res) => {
-  res.setHeader('Content-Type', 'text/css');
-  res.sendFile(path.join(__dirname, 'public', 'styles.css'));
+    res.setHeader('Content-Type', 'text/css');
+    res.sendFile(path.join(__dirname, 'public', 'styles.css'));
 });
+
+app.use('/favicon.ico', express.static('public/favicon.ico'));
 
 const inventory = JSON.parse(fs.readFileSync('inventory.json', 'utf-8'));
 
 app.get('/', (req, res) => {
-  res.render('index', { inventory });
+    res.render('index', { inventory });
 });
 
-/*
-app.post('/generate-list', (req, res) => {
-  const selectedItems = req.body.items;
-  const sortedItems = selectedItems.sort((a, b) => {
-    return inventory.find(x => x.item === a).aisle - inventory.find(x => x.item === b).aisle;
-  });
-  res.send({ sortedItems });
-});
-
-app.post('/generate-list', express.json(), (req, res) => {
-  const selectedItems = req.body.items;
-  const itemsWithAisles = selectedItems.map((item) => {
-    const inventoryItem = inventory.find((i) => i.item === item);
-    return { item, aisle: inventoryItem.aisle };
-  });
-  itemsWithAisles.sort((a, b) => parseInt(a.aisle) - parseInt(b.aisle));
-  const sortedItems = itemsWithAisles.map((item) => item.item);
-  res.json({ sortedItems });
-});
-*/
 app.post('/generate-list', (req, res) => {
   const selectedItems = req.body.items;
   const itemsWithAisles = selectedItems.map((item) => {
@@ -85,15 +114,6 @@ app.post('/generate-list', (req, res) => {
 
   res.render('list', { sortedItems });
 });
-
-/*
-app.post('/add-item', (req, res) => {
-  const newItem = req.body['new-item'];
-  const aisle = "Unknown"; // Set a default aisle for new items
-  inventory.push({ item: newItem, aisle: aisle });
-  res.redirect('/'); // Redirect back to the item selection page
-});
-*/
 
 app.post('/add-item', (req, res) => {
   const newItem = req.body['new-item'];
@@ -124,6 +144,7 @@ app.listen(myHTTP, () => {
 function backupInventory() {
   const timestamp = new Date().toISOString().replace(/[:.]/g, '-');
   const backupFile = `./backups/inventory-backup-${timestamp}.json`;
+
   fs.copyFile('./inventory.json', backupFile, (err) => {
     if (err) {
       console.error('Error creating backup:', err);
@@ -139,13 +160,12 @@ function backupInventory() {
 // - [ ] Pistachios
 // ..
 // - [ ] Dairy
-
 function saveListToFile(sortedItems) {
-    const timestamp = new Date().toISOString().replace(/[:.]/g, '-');
-    const fileName = `./lists/list-${timestamp}.txt`;
-    const today = new Date().toDateString();
+    //const timestamp = new Date().toISOString().replace(/[:.]/g, '-');
+    const fileName  = `./lists/list.txt`;
+    const today     = new Date().toDateString();
     
-    const fileContent = `* ${today} Groceries List\n\n` +sortedItems.reduce((content, item) => {
+    const fileContent = `* ${today} Groceries List\n\n` + sortedItems.reduce((content, item) => {
         return content + `- [ ] ${item}\n`;
     }, '');
 
@@ -154,6 +174,17 @@ function saveListToFile(sortedItems) {
             console.error('Error writing list to file:', err);
         } else {
             console.log('List saved to file:', fileName);
+
+            // Execute the external script with the new file as an argument
+            //exec(`./cav.sh ${fileName}`, (err, stdout, stderr) => {
+            exec(`cav.sh ${fileName}`, (err, stdout, stderr) => {
+                if (err) {
+                    console.error('Error executing cav.sh:', err);
+                    return;
+                }
+                if (stdout) console.log('cav.sh stdout:', stdout);
+                if (stderr) console.log('cav.sh stderr:', stderr);
+            });
         }
     });
 }
