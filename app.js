e68e9444fc3631df5530f27cb5e4a4151de9ef8c
Neil Cherry <linuxha@linuxha.com>
2023-04-09 22:15:46 -0400

    add delete item for testing
    Added a delete inventory item so I can clean up after the test case
    that adds an item. There currently is no 'Delete item' button on the
    web pages. Not sure how I want ot approach this yet.

--- app.js
+++ app.js
@@ -21,29 +21,26 @@ const express    = require('express');
 const bodyParser = require('body-parser');
 const fs         = require('fs');
 const app        = express();
-
-const { exec } = require('child_process');
+const path       = require('path');
+const https      = require('https');
+const { exec }   = require('child_process');
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-const path  = require('path');
-const https = require('https');
+const privateKeyPath  = path.join(__dirname, './ssl/key.pem');
+const certificatePath = path.join(__dirname, './ssl/cert.pem');
 
-// Read the SSL certificate files
-// Note: These files need to be provided by the user
-// Read the SSL certificate files
-const privateKeyPath  = './ssl/key.pem';
-const certificatePath = './ssl/cert.pem';
-// Mine are not self signed but I left this alone
-const caPath          = './ssl/cert.pem'; // For self-signed certificate, use the same cert.pem
+const hasSSLCredentials = fs.existsSync(privateKeyPath) && fs.existsSync(certificatePath);
 
-if (fs.existsSync(privateKeyPath) && fs.existsSync(certificatePath) && fs.existsSync(caPath)) {
+let httpsServer;
+
+if (hasSSLCredentials) {
     const privateKey  = fs.readFileSync(privateKeyPath, 'utf8');
     const certificate = fs.readFileSync(certificatePath, 'utf8');
-    const ca          = fs.readFileSync(caPath, 'utf8');
+    const ca          = fs.readFileSync(certificatePath, 'utf8');
 
     const credentials = {
         key:  privateKey,
@@ -51,115 +48,81 @@ if (fs.existsSync(privateKeyPath) && fs.existsSync(certificatePath) && fs.exists
         ca:   ca,
     };
 
-    // Create an HTTPS service
-    const httpsServer = https.createServer(credentials, app);
+    httpsServer = https.createServer(credentials, app);
 
-    // Set the HTTPS server to listen on a specific port (e.g., 8443)
     httpsServer.listen(myHTTPS, () => {
         console.log(`HTTPS server running on port ${myHTTPS}`);
     });
-} else {
-    console.log('SSL certificate files not found. Skipping HTTPS server.');
 }
-/*
-const privateKey  = fs.readFileSync('./ssl/key.pem', 'utf8');
-const certificate = fs.readFileSync('./ssl/cert.pem', 'utf8');
-
-const ca = fs.readFileSync('./ssl/cert.pem', 'utf8'); // For self-signed certificate, use the same cert.pem
-
-const credentials = {
-    key:  privateKey,
-    cert: certificate,
-    ca:   ca,
-};
-
-// Create an HTTPS service
-const httpsServer = https.createServer(credentials, app);
-
-// Set the HTTPS server to listen on a specific port (e.g., 8443)
-httpsServer.listen(myHTTPS, () => {
-    console.log(`HTTPS server running on port ${myHTTPS}`);
-});
-*/
-// ==============================================================================
-
-/*
-@FIXED: Refused to apply style from 'http://mozart.uucp:13000/styles.css' because its MIME type ('text/html') is not a supported stylesheet MIME type, and strict MIME checking is enabled.
-*/
 
 app.use('/styles.css', (req, res) => {
     res.setHeader('Content-Type', 'text/css');
     res.sendFile(path.join(__dirname, 'public', 'styles.css'));
 });
 
-app.use('/favicon.ico', express.static('public/favicon.ico'));
+app.use('/favicon.ico', (req, res) => {
+    res.setHeader('Content-Type', 'image/x-icon');
+    res.sendFile(path.join(__dirname, 'public', 'favicon.ico'));
+});
 
-const inventory = JSON.parse(fs.readFileSync('inventory.json', 'utf-8'));
+const inventory = JSON.parse(fs.readFileSync(path.join(__dirname, 'inventory.json'), 'utf-8'));
 
 app.get('/', (req, res) => {
     res.render('index', { inventory });
 });
 
 app.post('/generate-list', (req, res) => {
-  const selectedItems = req.body.items;
-  const itemsWithAisles = selectedItems.map((item) => {
-    const inventoryItem = inventory.find((i) => i.item === item);
-    return { item, aisle: inventoryItem.aisle };
-  });
-  itemsWithAisles.sort((a, b) => parseInt(a.aisle) - parseInt(b.aisle));
-  const sortedItems = itemsWithAisles.map((item) => item.item);
+    const selectedItems = req.body.items;
+    console.log(JSON.stringify(selectedItems));
 
-  // Save the sorted list to a file
-  saveListToFile(sortedItems);
+    const itemsWithAisles = selectedItems.map((item) => {
+        const inventoryItem = inventory.find((i) => i.item === item);
+        return { item, aisle: inventoryItem.aisle };
+    });
 
-  res.render('list', { sortedItems });
+    itemsWithAisles.sort((a, b) => parseInt(a.aisle) - parseInt(b.aisle));
+    const sortedItems = itemsWithAisles.map((item) => item.item);
+
+    // Save the sorted list to a file
+    saveListToFile(sortedItems);
+
+    res.render('list', { sortedItems });
 });
 
 app.post('/add-item', (req, res) => {
-  const newItem = req.body['new-item'];
-  const aisle   = req.body['aisle'];
-  inventory.push({ item: newItem, aisle: aisle });
+    const newItem = req.body['new-item'];
+    const aisle = req.body['aisle'];
+    inventory.push({ item: newItem, aisle: aisle });
 
-  // Backup the current inventory
-  backupInventory();
+    backupInventory();
 
-  // Update the inventory.json file
-  fs.writeFile('./inventory.json', JSON.stringify(inventory, null, 2), (err) => {
-    if (err) {
-      console.error('Error updating inventory:', err);
-      res.status(500).send('Error updating inventory.');
-    } else {
-      console.log('Inventory updated.');
-      res.redirect('/'); // Redirect back to the item selection page
-    }
-  });
+    fs.writeFile(path.join(__dirname, './inventory.json'), JSON.stringify(inventory, null, 2), (err) => {
+        if (err) {
+            console.error('Error updating inventory:', err);
+            res.status(500).send('Error updating inventory.');
+        } else {
+            console.log('Inventory updated.');
+            res.redirect('/');
+        }
+    });
 });
 
-app.listen(myHTTP, () => {
-  console.log(`Server started on port ${myHTTP}`);
+const httpServer = app.listen(myHTTP, () => {
+    console.log(`Server started on port ${myHTTP}`);
 });
-
-// ------------------------------------------------------------------------------
 
 function backupInventory() {
-  const timestamp = new Date().toISOString().replace(/[:.]/g, '-');
-  const backupFile = `./backups/inventory-backup-${timestamp}.json`;
-
-  fs.copyFile('./inventory.json', backupFile, (err) => {
-    if (err) {
-      console.error('Error creating backup:', err);
-    } else {
-      console.log('Backup created:', backupFile);
-    }
-  });
+    const timestamp = new Date().toISOString().replace(/[:.]/g, '-');
+    const backupFile = path.join(__dirname, `./backups/inventory-backup-${timestamp}.json`);
+    fs.copyFile(path.join(__dirname, './inventory.json'), backupFile, (err) => {
+        if (err) {
+            console.error('Error creating backup:', err);
+        } else {
+            console.log('Backup created:', backupFile);
+        }
+    });
 }
 
-// Format like this:
-// * 4/6/2023 Groceries list
-//
-// - [ ] Pistachios
-// ..
-// - [ ] Dairy
 function saveListToFile(sortedItems) {
     //const timestamp = new Date().toISOString().replace(/[:.]/g, '-');
     const fileName  = `./lists/list.txt`;
@@ -177,7 +140,7 @@ function saveListToFile(sortedItems) {
 
             // Execute the external script with the new file as an argument
             //exec(`./cav.sh ${fileName}`, (err, stdout, stderr) => {
-            exec(`cav.sh ${fileName}`, (err, stdout, stderr) => {
+            exec(`cav.sh ${fileName}> /tmp/cav.sh.log 2>&1`, (err, stdout, stderr) => {
                 if (err) {
                     console.error('Error executing cav.sh:', err);
                     return;
@@ -189,4 +152,30 @@ function saveListToFile(sortedItems) {
     });
 }
 
-module.exports = app;
+app.delete('/item/:itemName', (req, res) => {
+    const itemName = req.params.itemName;
+
+    const itemIndex = inventory.findIndex((i) => i.item === itemName);
+    if (itemIndex === -1) {
+        res.status(404).send('Item not found.');
+        return;
+    }
+
+    inventory.splice(itemIndex, 1);
+
+    // Backup the current inventory
+    backupInventory();
+
+    // Update the inventory.json file
+    fs.writeFile('./inventory.json', JSON.stringify(inventory, null, 2), (err) => {
+        if (err) {
+            console.error('Error updating inventory:', err);
+            res.status(500).send('Error updating inventory.');
+        } else {
+            console.log('Item deleted from inventory.');
+            res.sendStatus(200); // Send success status
+        }
+    });
+});
+
+module.exports = { app, httpServer, httpsServer }; // Export the servers
